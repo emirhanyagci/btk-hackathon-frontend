adab7d84882fd473ca6f041e8afa80eea94614ba
Emirhan Yagci <emirhan.yacis@gmail.com>
2024-11-02 17:22:22 +0300

    Add Auth Guard

--- app/AppWrappers.tsx
+++ app/AppWrappers.tsx
@@ -7,7 +7,17 @@ import '@/styles/MiniCalendar.css';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import theme from '@/theme/theme';
+import { UseAuthContext } from '@/contexts/AuthContext';
+import { redirect } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 export default function AppWrappers({ children }: { children: ReactNode }) {
+  const { value } = UseAuthContext();
+  const pathname = usePathname();
+  console.log(pathname);
+
+  const { isLoggedIn } = value;
+  if (!isLoggedIn && pathname !== '/login' && pathname !== '/signup')
+    return redirect('/login');
   return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 }
