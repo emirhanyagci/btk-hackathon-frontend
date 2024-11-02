30323cdec883ef7e7125167710848747e181382c
Emirhan Yagci <emirhan.yacis@gmail.com>
2024-11-02 17:08:35 +0300

    Add auth api routes

---
+++ app/api/signup/route.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export async function POST(req: Request): Promise<any> {
+  const data = await req.json();
+
+  const BACKEND_URL = process.env.BACKEND_URL;
+  if (!BACKEND_URL) {
+    throw new Error('BACKEND_URL is not set');
+  }
+
+  const res = await axios.post(`${BACKEND_URL}/user/register`, data);
+
+  return Response.json(res.data);
+}
